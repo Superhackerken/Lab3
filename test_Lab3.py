bf34da5b7fee1594b357409571908fc0c7a6a2c4
superken <CKENDRICSES06.24@ichat.sp.edu.sg>
2025-05-09 00:40:20 +0800

    Added a test system to test functions in lab 3!!

--- test_Lab3.py
+++ test_Lab3.py
@@ -21,6 +21,33 @@ def test_bubble_sort_descending():
 
     assert (result == test_arr)
 
+def test_bubble_sort_more_or_equal_10():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 10, 28, 49, 29]
+    test_arr = 1
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == test_arr)
+
+def test_bubble_sort_no_numbers():
+    result = []
+    input_arr = []
+    test_arr = 0
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == test_arr)
+
+def test_bubble_sort_not_int():
+    result = []
+    input_arr = [64, 34, 25, "BABA", 22, 11, 90]
+    test_arr = 2
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == test_arr)
+
 def test_bubble_sort_invalid():
     result = []
     input_arr = [64, 34, 25, 12, 22, 11, 90]
